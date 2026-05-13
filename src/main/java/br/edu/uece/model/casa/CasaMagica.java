4e3cf57fb2874f48a303d1b0c35bb0dd37ea689a
murilosousaz <murilosz085@gmail.com>
2026-05-13 14:07:28 -0300

    add das classes faltantes

--- src/main/java/br/edu/uece/model/casa/CasaMagica.java
+++ src/main/java/br/edu/uece/model/casa/CasaMagica.java
@@ -1,4 +1,23 @@
 package main.java.br.edu.uece.model.casa;
 
-public class CasaMagica {
+public class CasaMagica extends Casa {
+
+    public CasaMagica(int numero) {
+        super(numero);
+    }
+
+    @Override
+    public String getTipo() {
+        return "Mágica";
+    }
+
+    @Override
+    public String getDescricao() {
+        return "Casa Mágica! Troque de posição com o jogador que está em último lugar.";
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Casa %d [Mágica ⇄]", getNumero());
+    }
 }
--- src/main/java/br/edu/uece/model/casa/CasaSorte.java
+++ src/main/java/br/edu/uece/model/casa/CasaSorte.java
@@ -1,4 +1,29 @@
 package main.java.br.edu.uece.model.casa;
 
-public class CasaSorte {
+public class CasaSorte extends Casa {
+
+    private static final int CASAS_AVANCAR = 3;
+
+    public CasaSorte(int numero) {
+        super(numero);
+    }
+
+    public int getCasasAvancar() {
+        return CASAS_AVANCAR;
+    }
+
+    @Override
+    public String getTipo() {
+        return "Sorte";
+    }
+
+    @Override
+    public String getDescricao() {
+        return String.format("Casa da Sorte! Avance %d casas (não funciona para jogadores Azarados).", CASAS_AVANCAR);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Casa %d [Sorte +%d]", getNumero(), CASAS_AVANCAR);
+    }
 }
